e135e17ae1ea17ea37ee19bb740956846ba11829
Song <quanpengsong@gmail.com>
2021-05-23 11:18:11 +0800

    :tada: update bilibili & zhihu card style

--- render/bilibili.js
+++ render/bilibili.js
@@ -5,21 +5,30 @@ function renderBilibiliCard(data) {
     description = description.substr(0, lengthLimit);
     description += '...';
   }
-  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="300" viewBox="0 0 500 300">
+  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="382" height="210" viewBox="0 0 382 210">
   <defs>
-    <clipPath id="clip-Bilibili">
-      <rect width="500" height="300"/>
-    </clipPath>
+    <filter id="Card" x="0" y="0" width="382" height="210" filterUnits="userSpaceOnUse">
+      <feOffset dy="3" input="SourceAlpha"/>
+      <feGaussianBlur stdDeviation="3" result="blur"/>
+      <feFlood flood-opacity="0.161"/>
+      <feComposite operator="in" in2="blur"/>
+      <feComposite in="SourceGraphic"/>
+    </filter>
   </defs>
-  <g id="Bilibili" clip-path="url(#clip-Bilibili)">
-    <rect width="500" height="300" fill="#fff"/>
-    <text id="关注者数量_0" data-name="关注者数量：0" transform="translate(85 115)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">关注者数量：${fans}</tspan></text>
-    <text id="得到的赞同_0" data-name="得到的赞同：0" transform="translate(85 168)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">得到的赞同：${likes}</tspan></text>
-    <text id="视频播放量_0" data-name="视频播放量：0" transform="translate(284 115)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">视频播放量：${views}</tspan></text>
-    <text id="B_站等级_0" data-name="B 站等级：0" transform="translate(284 168)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">B 站等级：${level}</tspan></text>
-    <text id="个人简介_无" data-name="个人简介：无" transform="translate(85 221)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">个人简介：${description}</tspan></text>
-    <text id="JustSong_的_B_站数据" data-name="JustSong 的 B 站数据" transform="translate(118 50)" font-size="26" font-family="MicrosoftYaHei-Bold, Microsoft YaHei" font-weight="700"><tspan x="0" y="0">${name} 的 B 站数据</tspan></text>
+  <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Card)">
+    <rect id="Card-2" data-name="Card" width="364" height="192" rx="8" transform="translate(9 6)" fill="#fff"/>
   </g>
+  <text id="JustSong_s_Bilibili_Stats" data-name="JustSong&apos;s Bilibili Stats" transform="translate(94 44)" fill="#212121" font-size="18" font-family="SegoeUI-Bold, Segoe UI" font-weight="700"><tspan x="0" y="0">JustSong&apos;s Bilibili Stats</tspan></text>
+  <text id="Followers" transform="translate(55 84)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Followers</tspan></text>
+  <text id="Video_Views" data-name="Video Views" transform="translate(203 85)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Video Views</tspan></text>
+  <text id="Upvotes" transform="translate(55 119)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Upvotes</tspan></text>
+  <text id="Bilibili_Level" data-name="Bilibili Level" transform="translate(203 119)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Bilibili Level</tspan></text>
+  <text id="Signature" transform="translate(55 154)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Signature</tspan></text>
+  <text id="_0" data-name="0" transform="translate(126 84)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${fans}</tspan></text>
+  <text id="_0-2" data-name="0" transform="translate(126 119)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${likes}</tspan></text>
+  <text id="_0-3" data-name="0" transform="translate(289 86)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${views}</tspan></text>
+  <text id="_0-4" data-name="0" transform="translate(289 121)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${level}</tspan></text>
+  <text id="哈哈哈" transform="translate(126 155)" fill="#212121" font-size="13" font-family="YuGothicUI-Regular, Yu Gothic UI"><tspan x="0" y="0">${description}</tspan></text>
 </svg>
 `;
 }
--- render/zhihu.js
+++ render/zhihu.js
@@ -9,22 +9,32 @@ function renderZhihuCard(data) {
     question_count,
     articles_count,
   } = data;
-  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="300" viewBox="0 0 500 300">
+  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="382" height="210" viewBox="0 0 382 210">
   <defs>
-    <clipPath id="clip-Zhihu">
-      <rect width="500" height="300"/>
-    </clipPath>
+    <filter id="Card" x="0" y="0" width="382" height="210" filterUnits="userSpaceOnUse">
+      <feOffset dy="3" input="SourceAlpha"/>
+      <feGaussianBlur stdDeviation="3" result="blur"/>
+      <feFlood flood-opacity="0.161"/>
+      <feComposite operator="in" in2="blur"/>
+      <feComposite in="SourceGraphic"/>
+    </filter>
   </defs>
-  <g id="Zhihu" clip-path="url(#clip-Zhihu)">
-    <rect width="500" height="300" fill="#fff"/>
-    <text id="关注者数量_0" data-name="关注者数量：0" transform="translate(84 115)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">关注者数量：${follower_count}</tspan></text>
-    <text id="得到的赞同_0" data-name="得到的赞同：0" transform="translate(84 168)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">得到的赞同：${voteup_count}</tspan></text>
-    <text id="得到的喜欢_0" data-name="得到的喜欢：0" transform="translate(84 216)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">得到的喜欢：${thanked_count}</tspan></text>
-    <text id="问题数量_0" data-name="问题数量：0" transform="translate(284 115)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">问题数量：${question_count}</tspan></text>
-    <text id="回答数量_0" data-name="回答数量：0" transform="translate(284 168)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">回答数量：${answer_count}</tspan></text>
-    <text id="文章数量_0" data-name="文章数量：0" transform="translate(284 216)" font-size="18" font-family="MicrosoftYaHei, Microsoft YaHei"><tspan x="0" y="0">文章数量：${articles_count}</tspan></text>
-    <text id="JustSong_的知乎数据" data-name="JustSong 的知乎数据" transform="translate(118 50)" font-size="26" font-family="MicrosoftYaHei-Bold, Microsoft YaHei" font-weight="700"><tspan x="0" y="0">${name} 的知乎数据</tspan></text>
+  <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Card)">
+    <rect id="Card-2" data-name="Card" width="364" height="192" rx="8" transform="translate(9 6)" fill="#fff"/>
   </g>
+  <text id="JustSong_s_Zhihu_Stats" data-name="JustSong&apos;s Zhihu Stats" transform="translate(97 44)" fill="#212121" font-size="18" font-family="SegoeUI-Bold, Segoe UI" font-weight="700"><tspan x="0" y="0">${name}&apos;s Zhihu Stats</tspan></text>
+  <text id="Followers" transform="translate(63 83)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Followers</tspan></text>
+  <text id="Upvotes" transform="translate(63 120)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Upvotes</tspan></text>
+  <text id="Likes" transform="translate(63 157)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Likes</tspan></text>
+  <text id="Questions" transform="translate(220 83)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Questions</tspan></text>
+  <text id="Answers" transform="translate(221 120)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Answers</tspan></text>
+  <text id="Articles" transform="translate(220 157)" fill="#9e9e9e" font-size="13.5" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Articles</tspan></text>
+  <text id="_0" data-name="0" transform="translate(134 83)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${follower_count}</tspan></text>
+  <text id="_0-2" data-name="0" transform="translate(134 121)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${voteup_count}</tspan></text>
+  <text id="_0-3" data-name="0" transform="translate(134 159)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${thanked_count}</tspan></text>
+  <text id="_0-4" data-name="0" transform="translate(295 84)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${question_count}</tspan></text>
+  <text id="_0-5" data-name="0" transform="translate(295 121)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${answer_count}</tspan></text>
+  <text id="_0-6" data-name="0" transform="translate(295 158)" fill="#212121" font-size="15" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">${articles_count}</tspan></text>
 </svg>
 `;
 }
