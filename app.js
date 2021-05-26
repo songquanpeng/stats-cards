923dc4d1df43bd5e9d82dce87ac0513ca0f702fd
Jiwei,Yuan <ji-weiyuan@outlook.com>
2021-05-26 23:20:22 +0800

    feat(juejin): add junjin.cn stats

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const zhihu = require('./api/zhihu');
 const bilibili = require('./api/bilibili');
 const leetcode = require('./api/leetcode')
+const juejin = require('./api/juejin')
 
 app.use('/api/zhihu', zhihu);
 app.use('/api/bilibili', bilibili);
 app.use('/api/leetcode', leetcode);
+app.use('/api/juejin', juejin)
 
 const server = http.createServer(app);
 
--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 const getLeetCodeInfo = require('./crawer/leetcode');
+const getJuejinInfo = require('./crawer/juejin')
 
 async function test() {
-  let info = await getLeetCodeInfo("quanpeng");
-  console.log(info);
+  let leetCodeInfo = await getLeetCodeInfo("quanpeng");
+  console.log('leetCodeInfo', leetCodeInfo);
+
+  let juejinInfo = await getJuejinInfo('1556564194374926')
+  console.log('juejinInfo', juejinInfo)
 }
 
 test();
