fc57e3b1c9073f8a00bfa4c04dc3873944fba694
JustSong <songquanpeng@foxmail.com>
2022-12-15 21:57:25 +0800

    :zap: speed up bilibili crawler by using concurrent axios requests

--- crawer/bilibili.js
+++ crawer/bilibili.js
@@ -10,14 +10,30 @@ async function getBilibiliInfo(id) {
     fans: 0,
     likes: NaN,
     level: 0,
-    views: NaN,
+    views: NaN
   };
   try {
-    let res = await axios.get(
-      `https://m.bilibili.com/space/${id}`,
-      axiosConfig
-    );
-    let $ = cheerio.load(res.data);
+    let endPoints = [
+      {
+        url: `https://m.bilibili.com/space/${id}`,
+        config: axiosConfig
+      },
+      {
+        url: `https://api.bilibili.com/x/relation/stat?vmid=${id}`,
+        config: axiosConfig
+      },
+      {
+        url: `https://api.bilibili.com/x/space/upstat?mid=${id}`,
+        config: {
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36',
+            'Cookie': `SESSDATA=${process.env['BILIBILI_SESSDATA']}`
+          }
+        }
+      }
+    ];
+    let [res1, res2, res3] = await Promise.all(endPoints.map(endPoint => axios.get(endPoint.url, endPoint.config)));
+    let $ = cheerio.load(res1.data);
     $('.base .name').each((i, e) => {
       result.name = $(e).text();
     });
@@ -29,24 +45,16 @@ async function getBilibiliInfo(id) {
     $('.desc .content').each((i, e) => {
       result.description = $(e).text();
     });
-    res = await axios.get(
-      `https://api.bilibili.com/x/relation/stat?vmid=${id}`,
-      axiosConfig
-    );
-    let data = res.data.data;
+    let data = res2.data.data;
     result.fans = data.follower;
     result.following = data.following;
-    res = await axios.get(
-      `https://api.bilibili.com/x/space/upstat?mid=${id}`, {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36',
-          'Cookie': `SESSDATA=${process.env['BILIBILI_SESSDATA']}`
-        },
-      }
-    )
-    data = res.data.data;
-    result.views = data.archive.view;
-    result.likes = data.likes;
+    data = res3.data.data;
+    if (data?.archive?.view) {
+      result.views = data?.archive?.view;
+    }
+    if (data.likes) {
+      result.likes = data.likes;
+    }
   } catch (e) {
     console.error(e);
   }
