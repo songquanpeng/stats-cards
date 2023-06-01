5ef083fc2b45163f3faa7a3b514911d3c7ce6778
I-linmeng-I <48974182+I-linmeng-I@users.noreply.github.com>
2023-05-31 22:06:18 -0400

    :tada: add support for MoeCube MyCard (#15)
    * mycard
    
    * mycard
    
    * mycard
    
    * 1

---
+++ crawler/mycard.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+let config = {};
+if (process.env['GITHUB_TOKEN']) {
+    config = {
+        headers: { Authorization: `Bearer ${process.env['GITHUB_TOKEN']}` }
+    };
+}
+
+async function getMycardInfo(username) {
+    let result = {
+        name: "",
+        arena_rank: 0,
+        DP: 0,
+        athletic_wl_ratio: 0,
+        athletic_win: 0,
+        athletic_lose: 0
+    };
+
+    try {
+        result.name = username;
+        username = encodeURI(username);
+        let res = await axios.get(
+            `https://sapi.moecube.com:444/ygopro/arena/user?username=${username}`,
+            config
+        );
+        let userInfo = res.data;
+        result.arena_rank = userInfo.arena_rank;
+        result.DP = userInfo.pt;
+        result.athletic_wl_ratio = userInfo.athletic_wl_ratio;
+        result.athletic_win = userInfo.athletic_win;
+        result.athletic_lose = userInfo.athletic_lose;
+
+    } catch (e) {
+        console.error(e);
+    }
+    return result;
+}
+
+module.exports = getMycardInfo;
